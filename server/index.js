f403131b314f6e38497b6141eaebfe9c9730b837
Arpad <arpad@Arpads-MacBook-Pro.local>
2023-10-11 21:54:55 -0400

    format files to trigger build

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const morgan = require('morgan')
 const routes = require('./src/routes.ts')
 
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
@@ -16,7 +17,9 @@ app.use(morgan('dev'))
 routes(app, fs)
 
 dotenv.config()
+
 const { SERVER_PORT } = process.env
+
 app.listen(SERVER_PORT, () => {
   console.log(`[server]: Server is running at http://localhost:%s`, SERVER_PORT)
 })
